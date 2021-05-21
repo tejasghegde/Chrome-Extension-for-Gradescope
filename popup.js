79a6a4da86d7a89346da6535cfe964b98daabe93
Tejas Hegde <tejasghegde01@gmail.com>
2021-05-20 22:48:05 -0700

    redoing

--- popup.js
+++ popup.js
@@ -11,14 +11,17 @@ changeColor.addEventListener("click", async () => {
   
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: calculateScore,
+      function: setPageBackgroundColor,
     });
   });
   
   // The body of this function will be executed as a content script inside the
   // current page
-  function calculateScore() {
+  function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
       document.body.style.backgroundColor = color;
     });
   }
+
+// code found at https://stackoverflow.com/questions/11684454/getting-the-source-html-of-the-current-page-from-chrome-extension
+
