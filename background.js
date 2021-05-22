c56f4f06361f1a35bb5284384134ada1ed1ce229
Tejas Hegde <tejasghegde01@gmail.com>
2021-05-21 19:26:07 -0700

    redid using youtube example

---
+++ background.js
@@ -0,0 +1,64 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({
+        name: "Jack"
+    });
+});
+
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, 
+    tab) => {
+        if (changeInfo.status == "complete" && /^http/.test(tab.url)) {
+            chrome.scripting.insertCSS({
+                target: { tabId: tabId},
+                files: ["./foreground_styles.css"]
+            })
+                .then(() => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: tabId},
+                        files: ["./foreground.js"]
+                    })
+                        .then(() => {
+                            console.log("Injected foreground script")
+
+                            // chrome.tabs.sendMessage(tabId, {
+                            //     message: "change_name",
+                            //     payload: "John"
+                            // })
+                        });
+                })
+                .catch(err => console.log(err));
+        }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "get_name") {
+        chrome.storage.local.get("name", data => {
+            if (chrome.runtime.lastError) {
+                sendResponse({
+                    message: "fail"
+                });
+                return;
+            }
+            sendResponse({
+                message: "success",
+                payload: data.name
+            })
+        });
+
+        return true;
+    } else if (request.message === "change_name") {
+        chrome.storage.local.set({
+            name: request.payload
+        }, () => {
+            if (chrome.runtime.lastError) {
+                sendResponse({ message: "fail"});
+                return;
+            }
+
+            sendResponse({message: "success"});
+        })
+
+        return true;
+    }
+});
