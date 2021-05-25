dd931a6b24a86509f9f5e9d3e1598f96a01687f0
Tejas Hegde <tejasghegde01@gmail.com>
2021-05-24 17:22:52 -0700

    Have to split html row

--- foreground.js
+++ foreground.js
@@ -17,14 +17,15 @@ ce_main_container.appendChild(ce_button);
 
 document.querySelector("body").appendChild(ce_main_container);
 
-chrome.runtime.sendMessage({
-    message: "get_name"
-}, response => {
-    if (response.message == "success") {
-        ce_name.innerHTML = `Percentage: ${response.payload}`
-    }
-}); 
-
+if(typeof chrome.app.isInstalled!=='undefined'){
+    chrome.runtime.sendMessage({
+        message: "get_name"
+    }, response => {
+        if (response.message == "success") {
+            ce_name.innerHTML = `Percentage: 0%`
+        }
+    }); 
+}
 ce_button.addEventListener("click", () => {
     chrome.runtime.sendMessage({
         message: "change_name",
@@ -32,51 +33,52 @@ ce_button.addEventListener("click", () => {
     }, response => {
         if (response.message === "success") {
             var x = document.getElementsByClassName("submissionStatus--score");
-            var y = [];
+            var score_list = [];
             var i;
             for (i = 0; i < x.length; i++) {
-                y.push(x[i].innerHTML);
+                score_list.push(x[i].innerHTML);
             }
+
+            var row = document.getElementsByClassName("odd");
+            var tejas = [];
+            for (var i = 0; i < row.length; i++) {
+                var y = row[i].innerHTML.split("Submitted");
+                if (y === row[i].innerHTML) {
+                    var row1 = document.getElementsByClassName("submissionStatus--score");
+                }
+            }
+            //Proficiency Check 1A: Variable Attributes (Final Try)
+            //Submitted
+            ce_name.innerHTML = `Percentage: ${row[0]}`
 
             // code for parsing page based on user's input.
 
-            var list = document.getElementsByTagName("a");
-            var list_2 = []
-            var f;
-            for (f = 0; f < list.length; f++) {
-                list_2.push(list[f].innerHTML)
-            }
-            list_2.splice(0,13);
-            var q;
-            for (q = 0; q < 3; q++) {
-                list_2.pop();
-            }
-            var targets = [];
-            var k;
-            for (k = 0; k < list_2.length; k++) {
-                if (list_2[k].includes(ce_input.value) === true) {
-                    targets.push(k);
-                } else if (ce_input.value == "Homework") {
-                        if (list_2[k].includes("HW")) {
-                            targets.push(k);
-                        }
-                }
-            }
-
-            var numerator = 0;
-            var denominator = 0;
-            var j;
-            for (j = 0; j < targets.length; j++) {
-                var fraction = y[targets[j]].split(" ");
-                numerator += Number(fraction[0]);
-                denominator += Number(fraction[2]);
-            }
-            var answer = numerator/denominator;
-            answer = Math.round(answer * 10000)/100;
-            answer = answer.toString() + "%";
+            // var name_list = document.getElementsByClassName("table--primaryLink");
+            // var new_list = [];
+            // for (var j = 0; j < name_list.length; j++){
+            //     if(typeof score_list[j] !== 'undefined')
+            //         new_list.push([name_list[j],score_list[j]]) // push an array with [name, score]
+            // }
             
-
-            ce_name.innerHTML = `Percentage: ${answer}`  //${final}, ${ce_input.value}
+            // var targets = [];
+            // for (var i = 0; i < new_list.length; i++){
+            //     if (new_list[i][0].innerHTML.includes(ce_input.value)){
+            //         if (typeof new_list[i][1] !== 'undefined'){
+            //             targets.push(new_list[i][1])
+            //         }
+            //     }
+            // }
+            // var numerator = 0;
+            // var denominator = 0;
+            // for (var j = 0; j < targets.length; j++) {
+            //     var fraction = targets[j].split(" ");
+            //     numerator += Number(fraction[0]);
+            //     denominator += Number(fraction[2]);
+            // }
+            // var answer = numerator/denominator;
+            // answer = Math.round(answer * 10000)/100;
+            // answer = answer.toString() + "%";
+            // ce_name.innerHTML = `Percentage: ${targets}`
         }
     });
 });
