907ab77b2157abc6ef27e419a48d88490dc924d0
Tejas Hegde <tejasghegde01@gmail.com>
2021-06-03 00:40:41 -0700

    removed extra code

--- background.js
+++ background.js
@@ -4,50 +4,28 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, 
-//     tab) => {
-//         if (changeInfo.status == "complete" && /^http/.test(tab.url)) {
-//             chrome.scripting.insertCSS({
-//                 target: { tabId: tabId},
-//                 files: ["./button_styles.css"]   // ./foreground_styles
-//             })
-//                 .then(() => {
-//                     chrome.scripting.executeScript({
-//                         target: { tabId: tabId},
-//                         files: ["button_script.js"]   // ./foreground
-//                     })
-//                         .then(() => {
-//                             console.log("Injected button script")
-
-//                             // chrome.tabs.sendMessage(tabId, {
-//                             //     message: "change_name",
-//                             //     payload: "John"
-//                             // })
-//                         });
-//                 })
-//                 .catch(err => console.log(err));
-//         }
-// });
-
 chrome.action.onClicked.addListener((tabId) => {
-        console.log(tabId);
-        // if (changeInfo.status == "complete" && /^http/.test(tab.url)) {
-            chrome.scripting.insertCSS({
-                target: {tabId: tabId},
-                files: ["button_styles.css"]   // ./foreground_styles
+        // if (document.getElementById("ce_name") == undefined) {
+            chrome.scripting.executeScript({
+                target: {tabId: tabId.id},
+                files: ["./foreground.js"],
             })
                 .then(() => {
-                    chrome.scripting.executeScript({
-                        target: {tabId: tabId},
-                        files: ["button_script.js"]   // ./foreground
+                    chrome.scripting.insertCSS({
+                        target: {tabId: tabId.id},
+                        files: ["./foreground_styles.css"],   // ./foreground_styles
                     })
                         .then(() => {
                             console.log("Injected foreground script");
                         });
                 })
                 .catch(err => console.log(err));
+        // }
+        // else {
+        //     chrome.scripting.removeCSS({
+        //         target: {tabId: tabId.id},
+        //         files: ["./foreground_styles.css"]
+        //     })
         // }
 });
 
