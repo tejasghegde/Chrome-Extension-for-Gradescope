959e944407daadb30166418baca4d4c3daed9d3f
Tejas Hegde <tejasghegde01@gmail.com>
2021-06-13 00:12:27 -0700

    deleted unnessecary code

--- background.js
+++ background.js
@@ -1,58 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
-        name: "0%"
-    });
-});
-
 chrome.action.onClicked.addListener((tabId) => {
         chrome.scripting.executeScript({
             target: {tabId: tabId.id},
             files: ["foreground.js"],
-        })
-        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-            if (request.message !== "remove") {
+        },
+        (injectionResults) => {
+            if (chrome.runtime.lastError || 
+                !injectionResults || !injectionResults.length) {
+                return;
+            }
+            if (injectionResults[0].result !== true) {
                 chrome.scripting.insertCSS({
                     target: {tabId: tabId.id},
                     files: ["foreground_styles.css"]
                 })
-            } else if (request.message === "remove"){
-                console.log("target hit");
+            }
+            else if (injectionResults[0].result === true) {
                 chrome.scripting.removeCSS({
                     target: {tabId: tabId.id},
-                    files: ["foreground_styles.css"],
+                    files: ["foreground_styles.css"]
                 })
-                console.log("done");
             }
-        })
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "get_name") {
-        chrome.storage.local.get("name", data => {
-            if (chrome.runtime.lastError) {
-                sendResponse({
-                    message: "fail"
-                });
-                return;
-            }
-            sendResponse({
-                message: "success",
-                payload: data.name
-            })
+              console.log(injectionResults);
         });
-
-        return true;
-    } else if (request.message === "change_name") {
-        chrome.storage.local.set({
-            name: request.payload
-        }, () => {
-            if (chrome.runtime.lastError) {
-                sendResponse({ message: "fail"});
-                return;
-            }
-            sendResponse({message: "success"});
-        })
-
-        return true;
-    }
 });
